3f1a2124cb4643f96318101a1bd2006007bf43e7
bangot <dangvanbang1802@gmail.com>
2020-12-14 16:00:16 +0700

    suasai

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,14 @@ import java.util.Scanner;
 public class Main implements Serializable {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Manager business = new Manager();
+        Manager manager = new Manager();
         int cases = 0;
         do {
 
             System.out.println("quan ly danh ba ");
             System.out.println("chon chuc nang");
             System.out.println("1 : them moi danh ba");
-            System.out.println("2 : xem danh bạ");
+            System.out.println("2 : xem danh ba");
             System.out.println("3 : xoa");
             System.out.println("4 : tim kiem ");
             System.out.println("5 : sua thong tin");
@@ -20,23 +20,23 @@ public class Main implements Serializable {
                 cases = Integer.parseInt(sc.nextLine());
                 switch (cases) {
                     case 1:
-                        business.addInfos();
+                        manager.addInfos();
                         break;
                     case 2:
-                        business.show();
+                        manager.show();
                         break;
                     case 3:
-                        business.delete();
+                        manager.delete();
                         break;
                     case 4:
-                        business.search();
+                        manager.search();
                         break;
                     case 5:
-                        business.edit();
+                        manager.edit();
                         break;
                 }
             } catch (Exception e) {
-                System.out.println("nhập lại  ");
+                System.out.println("nhap lai  ");
             }
         } while (cases != 6);
     }
--- src/PhoneBook.java
+++ src/PhoneBook.java
@@ -1,4 +1,3 @@
-
 public class PhoneBook {
     private String numberPhone;
     private String phonebook;
